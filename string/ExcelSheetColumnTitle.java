66c07192be49eae2f01e642c55e089726b609d1e
zangsy <fbhduckpears@gmail.com>
2019-04-06 20:42:55 +0900

    Add comments to 168

--- string/ExcelSheetColumnTitle.java
+++ string/ExcelSheetColumnTitle.java
@@ -2,6 +2,11 @@
  * 168
  * 本质上就是将一个10进制数转换为一个26进制的数.
  * 由于下标从1开始而不是从0开始，因此要减一操作.
+ * 一个X进制的数abc，转换成10进制的话可以写成
+ * a*X^2 + b*X^1 + c*X^0，反过来的话，用这个数
+ * 对X取余，可以得到c，然后让这个数减去c再除以X，
+ * 这个数就变成a*X^1 + b*X^0，这时候继续对X取余
+ * 就可以得到b，一直重复直到这个数小于等于0.
  */
 public class ExcelSheetColumnTitle {
     public String convertToTitle(int n) {
